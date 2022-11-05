e5731acf503676db9a5866c8ac2c33fece1b2d43
entest <hai@entest.io>
2022-11-05 14:30:48 +0000

    first

--- lib/opensearch-lambda-stack.ts
+++ lib/opensearch-lambda-stack.ts
@@ -1,16 +1,28 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Duration, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
+
+interface OpenSearchLambdaProps extends StackProps {
+  opensearchDomain: string;
+}
 
 export class OpensearchLambdaStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: OpenSearchLambdaProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OpensearchLambdaQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.aws_lambda.Function(this, "TestOpenSearchLambda", {
+      functionName: "TestOpenSearchLambda",
+      handler: "index.handler",
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_9,
+      memorySize: 512,
+      timeout: Duration.seconds(10),
+      code: cdk.aws_lambda.Code.fromAsset(path.join(__dirname, "./../lambda")),
+      environment: {
+        OPENSEARCH_DOMAIN: props.opensearchDomain,
+        PYTHONPATH: "/var/task/package",
+        REGION: this.region,
+      },
+    });
   }
 }
